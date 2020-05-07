ea8c18418d0ca98062b8fca23907949630876c11
IsaacACasanova <isaacacasanova@gmail.com>
2020-05-06 21:50:24 -0400

    Add initial SearchTweetsViewSpec with test

--- TwitterCloneAppTests/SearchTweetsViewSpec.swift
+++ TwitterCloneAppTests/SearchTweetsViewSpec.swift
@@ -8,5 +8,40 @@
 
 import Nimble
 import Quick
+import ViewInspector
+@testable import TwitterCloneApp
 
-class SearchTweetsViewSpec: QuickSpec { }
+extension SearchTweetsView: Inspectable { }
+
+class SearchTweetsViewSpec: QuickSpec {
+    var subject: SearchTweetsView!
+}
+
+extension SearchTweetsViewSpec {
+    override func spec() {
+        beforeEach { self.subject = self.newSubject }
+        describe("searchField") {
+            it("displays the proper default text") {
+                let searchFieldText = try! self.subject
+                    .inspect()
+                    .view(SearchTweetsView.self)
+                    .navigationView()
+                    .list(0)
+                    .hStack(0)
+                    .textField(0)
+                    .text()
+                    .string()
+                expect(searchFieldText).to(equal("Search Tweets"))
+            }
+        }
+    }
+}
+
+// MARK: - Helpers
+private extension SearchTweetsViewSpec {
+    var newSubject: SearchTweetsView {
+        let mockService = MockTwitterService()
+        let viewModel = SearchTweetsViewModel(twitterService: mockService)
+        return SearchTweetsView(viewModel: viewModel)
+    }
+}
